48e69360b23c5df585a5e067b58c0f0418d84721
NadyaGus <ftotkexit@gmail.com>
2024-10-15 22:29:10 +0300

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
 import { type ReactElement, useState } from 'react';
 
+import { Container } from '@mantine/core';
+
+import type { Todo } from './types/todo';
+
 import { AddTodoForm } from './components/form';
 import { TodoList } from './components/list';
+import { Navigation } from './components/navigation/navigation';
 import { todosList } from './utils/todos-list';
 
 function App(): ReactElement {
-  const [list, setList] = useState(todosList);
+  const [list, setList] = useState<Todo[]>(todosList);
+  const [filter, setFilter] = useState<'active' | 'all' | 'completed'>('all');
+
   const addTodo = (title: string): void => {
     setList([...list, { completed: false, id: list.length + 1, title }]);
   };
 
+  const filterTodo = (filter: 'active' | 'all' | 'completed'): Todo[] => {
+    switch (filter) {
+      case 'active':
+        return list.filter((todo) => !todo.completed);
+      case 'completed':
+        return list.filter((todo) => todo.completed);
+      default:
+        return list;
+    }
+  };
+
+  const toggleTodo = (id: number): void => {
+    setList(
+      list.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      }),
+    );
+  };
+
+  const deleteCompleted = (): void => {
+    setList(list.filter((todo) => !todo.completed));
+  };
+
   return (
     <div className="App">
-      <h1>todos</h1>
-      <AddTodoForm addTodo={addTodo} />
-      <TodoList list={list} />
+      <Container maw={800}>
+        <h1>todos</h1>
+        <AddTodoForm addTodo={addTodo} />
+        <TodoList list={filterTodo(filter)} toggleTodo={toggleTodo} />
+        <Navigation deleteCompleted={deleteCompleted} filterTodo={setFilter} tasksCount={filterTodo('active').length} />
+      </Container>
     </div>
   );
 }
--- src/components/list/TodoList.tsx
+++ src/components/list/TodoList.tsx
@@ -6,7 +6,7 @@ import type { Todo } from '../../types/todo';
 
 import { Item } from '../item';
 
-export const TodoList = ({ list }: { list: Todo[] }): ReactElement => {
+export const TodoList = ({ list, toggleTodo }: { list: Todo[]; toggleTodo: (id: number) => void }): ReactElement => {
   return (
     <List listStyleType="none" m="md">
       {list.map((todo) => (
@@ -14,7 +14,7 @@ export const TodoList = ({ list }: { list: Todo[] }): ReactElement => {
           key={todo.id}
           todo={todo}
           toggleTodo={() => {
-            console.log(`Toggle ${todo.title}`);
+            toggleTodo(todo.id);
           }}
         />
       ))}
