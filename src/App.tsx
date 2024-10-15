6a5089ffb41381800ce5ef1c7918ff672ac724d4
NadyaGus <ftotkexit@gmail.com>
2024-10-15 21:51:44 +0300

    feat: add ability to adding task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import type { ReactElement } from 'react';
+import { type ReactElement, useState } from 'react';
 
 import { AddTodoForm } from './components/form';
 import { TodoList } from './components/list';
 import { todosList } from './utils/todos-list';
 
 function App(): ReactElement {
+  const [list, setList] = useState(todosList);
+  const addTodo = (title: string): void => {
+    setList([...list, { completed: false, id: list.length + 1, title }]);
+  };
+
   return (
     <div className="App">
       <h1>todos</h1>
-      <AddTodoForm />
-      <TodoList list={todosList} />
+      <AddTodoForm addTodo={addTodo} />
+      <TodoList list={list} />
     </div>
   );
 }
--- src/components/form/AddTodoForm.tsx
+++ src/components/form/AddTodoForm.tsx
@@ -5,26 +5,25 @@ import { Form, useForm } from '@mantine/form';
 
 import classes from './index.module.css';
 
-export const AddTodoForm = (): ReactElement => {
+export const AddTodoForm = ({ addTodo }: { addTodo: (title: string) => void }): ReactElement => {
   const form = useForm({
     initialValues: {
       title: '',
     },
-    mode: 'uncontrolled',
     validate: {
       title: (value) => (value ? null : 'Название задачи обязательно'),
     },
   });
 
   const handleFormSubmit = (values: { title: string }): void => {
-    console.log(values);
+    addTodo(values.title);
     form.reset();
   };
 
   return (
     <Container my={'auto'}>
       <Form className={classes.form} form={form} onSubmit={(values) => handleFormSubmit(values)}>
-        <TextInput key={form.values.title} placeholder="Название задачи" {...form.getInputProps('title')} flex={1} />
+        <TextInput placeholder="Название задачи" {...form.getInputProps('title')} flex={1} />
         <Button type="submit">Добавить задачу</Button>
       </Form>
     </Container>
